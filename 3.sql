f7f0433d8c73e9abf03dec0059106825aa0b3104
GMAtheCreative <timothywonder01@gmail.com>
2024-11-01 08:27:30 +0100

    progress

---
+++ 3.sql
@@ -0,0 +1,3 @@
+SELECT * FROM cape_codd.SKU_DATA WHERE SKU_Description LIKE 'Tent%';
+SELECT * FROM cape_codd.SKU_DATA WHERE SKU_Description LIKE '%Tent';
+SELECT * FROM cape_codd.SKU_DATA WHERE SKU_Description LIKE '%Tent%';
---
+++ class_work.sql
@@ -0,0 +1,22 @@
+-- SELECT SKU, SKU_Description, Department, Buyer
+-- FROM SKU_DATA;
+
+
+
+-- SELECT Department, Buyer
+-- FROM SKU_DATA;
+
+
+-- SELECT Buyer, Department
+-- FROM SKU_DATA;
+
+-- SELECT distinct Buyer, Department
+-- FROM SKU_DATA
+
+-- SELECT Buyer, Department
+-- FROM SKU_DATA
+-- LIMIT 5;
+
+SELECT *
+FROM SKU_DATA
+WHERE DEPARTMENT = "WATER SPORTS";
